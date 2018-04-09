1bb232e7bb9ec4bdafb502f12e239422ceaa5c36
dwangsg <david.wang.sg@hotmail.com>
2018-04-09 22:43:19 +0800

    test conflict

--- src/main/java/com/test/repository/FundDetailRepository.java
+++ src/main/java/com/test/repository/FundDetailRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface FundDetailRepository extends JpaRepository <FundDetail,Long>{
 
     FundDetail findByMStarID(String mStarId);
+
 }
